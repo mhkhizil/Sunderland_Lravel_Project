73d9bf7c0f686913a21cad625077c16f16a1e9ea
Thurein Zaw <109714248+mhkhizil@users.noreply.github.com>
2024-03-06 16:10:52 +0630

    adding model,controller ,grouping middleware and making route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +20,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::middleware("auth")->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('article', ArticleController::class);
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
